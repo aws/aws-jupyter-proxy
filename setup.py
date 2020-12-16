de2cac1d3c365a63a19e0c942c13b1967d68ea41
Jaipreet Singh <43826141+jaipreet-s@users.noreply.github.com>
2020-12-16 14:25:38 -0800

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aws_jupyter_proxy",
-    version="0.1.0",
+    version="0.2.0",
     url="https://github.com/aws/aws-jupyter-proxy",
     author="Amazon Web Services",
     description="A Jupyter server extension to proxy requests with AWS SigV4 authentication",
